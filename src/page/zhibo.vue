54622032f36951cd666c7b513a800f63f3e16d56
lafamenta <1749478071@qq.com>
2020-06-12 11:35:10 +0800

    直播页面web头部完成

--- src/page/zhibo.vue
+++ src/page/zhibo.vue
@@ -4,7 +4,7 @@
  * @Author: JohnsonZzp
  * @Date: 2020-06-11 15:30:09
  * @LastEditors: JohnsonZzp
- * @LastEditTime: 2020-06-11 18:22:06
+ * @LastEditTime: 2020-06-12 11:33:06
 -->
 <template>
   <div class="container">
@@ -18,14 +18,16 @@
               <div class="dec">
                 以追求极致用户体验为目标，持续更新系统功能
               </div>
-              <ul>
-                <li>
-
+              <ul class="list">
+                <li class="item" v-for="(item,index) in dianbo" :key="index" @mouseout="chooseDemo(index)" :style="{backgroundColor:(activeTop == index)?'#fff':''}">
+                  <img :src="activeTop == index?item.iconBlue:item.iconWhite" alt="">
+                  <span :style="{color:(activeTop == index)?'#000':'#fff'}">{{item.title}}</span>
                 </li>
               </ul>
             </div>
-            <div class="rihgt">
-
+            <div class="right">
+              <img :src='dianbo[activeTop].demo' alt="" class="demo">
+              <img src="../../static/image-zhibo/web/sjk.png" alt="" class="kuan">
             </div>
           </div>
         </div>
@@ -40,6 +42,7 @@ import '../../static/js/flexible'
 export default {
   data () {
     return {
+      activeTop:0,
       dianbo:[
         {
           iconWhite:'../../static/image-zhibo/web/gntb7.png',
@@ -54,64 +57,64 @@ export default {
           demo:'../../static/image-zhibo/web/lianmai.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb7.png',
-          iconBlue:'../../static/image-zhibo/web/gntb07.png',
+          iconWhite:'../../static/image-zhibo/web/gntb3.png',
+          iconBlue:'../../static/image-zhibo/web/gntb03.png',
           title:'直播带货',
-          demo:'../../static/image-zhibo/web/daihuo.gif'
+          demo:'../../static/image-zhibo/web/dongtai.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb1.png',
-          iconBlue:'../../static/image-zhibo/web/gntb01.png',
+          iconWhite:'../../static/image-zhibo/web/gntb4.png',
+          iconBlue:'../../static/image-zhibo/web/gntb04.png',
           title:'直播连麦PK',
-          demo:'../../static/image-zhibo/web/lianmai.gif'
+          demo:'../../static/image-zhibo/web/zhuanpan.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb7.png',
-          iconBlue:'../../static/image-zhibo/web/gntb07.png',
+          iconWhite:'../../static/image-zhibo/web/gntb5.png',
+          iconBlue:'../../static/image-zhibo/web/gntb05.png',
           title:'直播带货',
-          demo:'../../static/image-zhibo/web/daihuo.gif'
+          demo:'../../static/image-zhibo/web/liwu.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb1.png',
-          iconBlue:'../../static/image-zhibo/web/gntb01.png',
+          iconWhite:'../../static/image-zhibo/web/gntb6.png',
+          iconBlue:'../../static/image-zhibo/web/gntb06.png',
           title:'直播连麦PK',
-          demo:'../../static/image-zhibo/web/lianmai.gif'
+          demo:'../../static/image-zhibo/web/shouhu.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb7.png',
-          iconBlue:'../../static/image-zhibo/web/gntb07.png',
+          iconWhite:'../../static/image-zhibo/web/gntb2.png',
+          iconBlue:'../../static/image-zhibo/web/gntb02.png',
           title:'直播带货',
-          demo:'../../static/image-zhibo/web/daihuo.gif'
+          demo:'../../static/image-zhibo/web/xiaoshipin.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb1.png',
-          iconBlue:'../../static/image-zhibo/web/gntb01.png',
+          iconWhite:'../../static/image-zhibo/web/gntb8.png',
+          iconBlue:'../../static/image-zhibo/web/gntb08.png',
           title:'直播连麦PK',
-          demo:'../../static/image-zhibo/web/lianmai.gif'
+          demo:'../../static/image-zhibo/web/zhibo.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb7.png',
-          iconBlue:'../../static/image-zhibo/web/gntb07.png',
+          iconWhite:'../../static/image-zhibo/web/gntb9.png',
+          iconBlue:'../../static/image-zhibo/web/gntb09.png',
           title:'直播带货',
-          demo:'../../static/image-zhibo/web/daihuo.gif'
+          demo:'../../static/image-zhibo/web/fenxiao.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb1.png',
-          iconBlue:'../../static/image-zhibo/web/gntb01.png',
+          iconWhite:'../../static/image-zhibo/web/gntb10.png',
+          iconBlue:'../../static/image-zhibo/web/gntb010.png',
           title:'直播连麦PK',
-          demo:'../../static/image-zhibo/web/lianmai.gif'
+          demo:'../../static/image-zhibo/web/danmu.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb7.png',
-          iconBlue:'../../static/image-zhibo/web/gntb07.png',
+          iconWhite:'../../static/image-zhibo/web/gntb11.png',
+          iconBlue:'../../static/image-zhibo/web/gntb011.png',
           title:'直播带货',
-          demo:'../../static/image-zhibo/web/daihuo.gif'
+          demo:'../../static/image-zhibo/web/dashang.gif'
         },
         {
-          iconWhite:'../../static/image-zhibo/web/gntb1.png',
-          iconBlue:'../../static/image-zhibo/web/gntb01.png',
+          iconWhite:'../../static/image-zhibo/web/gntb12.png',
+          iconBlue:'../../static/image-zhibo/web/gntb012.png',
           title:'直播连麦PK',
-          demo:'../../static/image-zhibo/web/lianmai.gif'
+          demo:'../../static/image-zhibo/web/pindao.gif'
         },
       ]
     };
@@ -121,7 +124,11 @@ export default {
 
   computed: {},
 
-  methods: {}
+  methods: {
+    chooseDemo(index){
+      this.activeTop = index;
+    }
+  }
 }
 
 </script>
